0f33790cbd6e3e9f3e26c046526d85adb8fd2721
Kellen Busby <kellenbusby@gmail.com>
2023-10-07 13:05:38 -0600

    adding plausible analytics

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react'
 import styles from './tailwind.css'
 import favicons from './data/favicons'
@@ -15,7 +16,17 @@ export const links: LinksFunction = () => [
   ...favicons,
 ]
 
+export function loader() {
+  return {
+    ENV: {
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  }
+}
+
 export default function App() {
+  const loaderData = useLoaderData()
+
   return (
     <html lang="en">
       <head>
@@ -27,11 +38,24 @@ export default function App() {
       <body className="flex min-h-screen w-screen flex-col overflow-x-hidden bg-theme-gray-default text-theme-white">
         <Outlet />
         <ScrollRestoration />
-        <Scripts />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(loaderData.ENV)}`,
+          }}
+        />
         <script
           type="text/javascript"
           src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=c1e670aa50272d0fb69a8a3941c938cb36b9fd3a"
         ></script>
+        {process.env.NODE_ENV === 'production' && (
+          <script
+            defer
+            src="/stats/js/script.js"
+            data-api="/stats/api/event"
+            data-domain="kellenbusby.com"
+          ></script>
+        )}
+        <Scripts />
         <LiveReload />
       </body>
     </html>
