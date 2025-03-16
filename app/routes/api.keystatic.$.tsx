b857bf512e85becf6ad0900905ff74f721544a7b
Kellen Busby <kellenbusby@gmail.com>
2025-03-16 11:57:09 -0700

    removing keystatic in production

--- app/routes/api.keystatic.$.tsx
+++ app/routes/api.keystatic.$.tsx
@@ -2,5 +2,17 @@ import type { ActionFunction, LoaderFunction } from '@remix-run/node'
 import { handleLoader } from '@keystatic/remix/api'
 import config from '../../keystatic.config'
 
-export const loader: LoaderFunction = (args) => handleLoader({ config }, args)
-export const action: ActionFunction = (args) => handleLoader({ config }, args)
+export const loader: LoaderFunction = (args) => {
+  if (process.env.NODE_ENV === 'production') {
+    throw new Response('Not found', { status: 404 })
+  }
+
+  return handleLoader({ config }, args)
+}
+export const action: ActionFunction = (args) => {
+  if (process.env.NODE_ENV === 'production') {
+    return null
+  }
+
+  return handleLoader({ config }, args)
+}
--- app/routes/keystatic.$.tsx
+++ app/routes/keystatic.$.tsx
@@ -1,4 +1,12 @@
 import { makePage } from '@keystatic/remix/ui'
 import config from '../../keystatic.config'
 
+export function loader() {
+  if (process.env.NODE_ENV === 'production') {
+    throw new Response('Not found', { status: 404 })
+  }
+
+  return
+}
+
 export default makePage(config)
