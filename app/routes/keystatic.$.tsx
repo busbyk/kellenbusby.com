70c1bf97ea8ed0ae274cba994bfee9af9d727f31
Kellen Busby <kellenbusby@gmail.com>
2025-04-16 21:39:13 -0700

    enabling keystatic in production

--- app/routes/keystatic.$.tsx
+++ app/routes/keystatic.$.tsx
@@ -2,9 +2,9 @@ import { makePage } from '@keystatic/remix/ui'
 import config from '../../keystatic.config'
 
 export function loader() {
-  if (process.env.NODE_ENV === 'production') {
-    throw new Response('Not found', { status: 404 })
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   throw new Response('Not found', { status: 404 })
+  // }
 
   return null
 }
