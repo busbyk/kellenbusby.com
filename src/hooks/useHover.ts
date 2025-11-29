d4cd13dd40b9b7348c3d4d48b78530b25e8d190f
Kellen Busby <kellenbusby@gmail.com>
2025-11-28 18:25:29 -0800

    fixing ts

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,9 +1,9 @@
 import type { RefObject } from 'preact';
 import { useEffect, useRef, useState } from 'preact/hooks';
 
-export function useHover<T extends HTMLElement>(): [RefObject<T | null>, boolean] {
+export function useHover<T extends HTMLElement>(): [RefObject<T>, boolean] {
   const [value, setValue] = useState<boolean>(false);
-  const ref = useRef<T | null>(null);
+  const ref = useRef<T>(null);
   const handleMouseOver = (): void => setValue(true);
   const handleMouseOut = (): void => setValue(false);
 
